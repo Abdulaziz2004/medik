d8d31856268170aed6b6e824600761854e2ef261
xojaaa <96516074+xojaaa@users.noreply.github.com>
2022-04-16 17:26:12 +0300

    sdfsd

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,11 +8,39 @@ import Shifokorlar from "./shifokorlar";
 const HomePage = () => {
   return (
     <>
-      
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-2">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <NavLink class="nav-link" to="shifokorlar">
+                  <button className="btn btn-success">Shifokorlar <span className="badge badge-light">12</span></button>
+                </NavLink>
+              </li>
+              <li class="nav-item">
+                <NavLink class="nav-link" to="xizmatlar">
+                  <button className="btn btn-success">Xizmatlar</button>
+                </NavLink>
+              </li>
+              <li class="nav-item">
+                <NavLink class="nav-link" to="yonalish">
+                  <button className="btn btn-success">Yonalish</button>
+                </NavLink>
+              </li>
+              <li class="nav-item">
+                <NavLink class="nav-link" to="#">
+                  <button className="btn btn-success"> Link4</button>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="col-lg-10">
+            <Outlet />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
 
 export default HomePage;
-
-
